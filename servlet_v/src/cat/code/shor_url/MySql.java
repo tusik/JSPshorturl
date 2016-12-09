c5e8eb088f7a2190a86f59baf36096eb3520533e
image <onenookone@gmail.com>
2016-12-09 16:28:00 +0800

    servlet version

---
+++ servlet_v/src/cat/code/shor_url/MySql.java
@@ -0,0 +1,103 @@
+package cat.code.shor_url;
+
+
+import java.sql.*;
+
+/**
+ * Created by zinc on 2016/10/22.
+ *
+ * @author Image
+ */
+public class MySql {
+    ConfigLoader CL = new ConfigLoader();
+    public final String url = "jdbc:mysql://localhost/" +
+            CL.GetValueByKey("dbname")+"?useUnicode=true&characterEncoding=utf-8";
+    public final String name = "com.mysql.jdbc.Driver";
+    public final String user = CL.GetValueByKey("dbuser");
+    public final String password = CL.GetValueByKey("dbpw");
+    public Connection conn = null;
+    public PreparedStatement pst = null;
+    String sql = null;
+
+    public MySql() {}
+    public MySql(String sql, String[] vals) {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url, user, password);//获取连接
+            pst = conn.prepareStatement(sql);//准备执行语句
+            for(int i=0;i<=vals.length;i++){
+                pst.setString(i+1,vals[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public MySql(String sql, String val) {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url, user, password);//获取连接
+            pst = conn.prepareStatement(sql);//准备执行语句
+            pst.setString(1,val);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insert(String sql) {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url, user, password);//获取连接
+            pst = conn.prepareStatement(sql);//准备执行语句
+        } catch (Exception e) {
+            e.printStackTrace();
+        }//
+    }
+    public void insert(String sql,String[] vals) {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url, user, password);//获取连接
+            pst = conn.prepareStatement(sql);//准备执行语句
+            for(int i=0;i<vals.length;i++){
+                pst.setString(i+1,vals[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void insert(String sql,String val) {
+        try {
+            Class.forName(name);//指定连接类型
+            conn = DriverManager.getConnection(url, user, password);//获取连接
+            pst = conn.prepareStatement(sql);//准备执行语句
+            pst.setString(1,val);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            if(this.conn!=null){
+                this.conn.close();
+            }
+            if(this.pst!=null){
+                this.pst.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void close(ResultSet rs) {
+        close();
+        if(rs!=null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
